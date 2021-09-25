ec4925bb6b106fa173007c18db6cb68030aebda8
maurodegaspari <mauro_degaspari@hotmail.com>
2021-09-25 19:22:56 -0300

    projeto inicial

---
+++ src/test/java/br/com/maurodev/administor/AdministorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maurodev.administor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
