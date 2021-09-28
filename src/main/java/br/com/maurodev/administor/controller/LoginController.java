385676bf3c08a2297f7cf2d70242bca77945028c
maurodegaspari <mauro_degaspari@hotmail.com>
2021-09-28 15:50:20 -0300

    SQL INICIAL

--- src/main/java/br/com/maurodev/administor/controller/LoginController.java
+++ src/main/java/br/com/maurodev/administor/controller/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-	
+	//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
 
 	@RequestMapping("/login")
 	public String login(){
